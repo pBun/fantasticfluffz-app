7db02c8e9358bc9a099b0907505e8923c68f2997
Dustin Boersma <dustin.boersma@activision.com>
2022-02-05 02:55:02 -0800

    Updates

--- src/util/eth.js
+++
@@ -1,7 +0,0 @@
-export function weiToEth(val) {
-  return val / 1000000000000000000;
-}
-
-export default {
-  weiToEth,
-};
--- src/util/web3Address.js
+++
@@ -1,5 +0,0 @@
-export function prettyAddress(address) {
-  if (!address || address.length < 12) return address;
-  return `${address.slice(0, 10)}...${address.slice(-10)}`;
-}
-export default { prettyAddress };
